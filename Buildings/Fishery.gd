4ff7e7df90ded83c5cc6ae0fbac4cf3d75b90eb1
joesturge <joe.mike.sturge@gmail.com>
2024-04-15 14:56:11 +0100

    fixed pathing added debug boxes and implemented fishery and cat center

--- Buildings/Fishery.gd
+++ Buildings/Fishery.gd
@@ -10,13 +10,13 @@ func _process(delta):
 	
 func do_build():
 	super.do_build()
-	change_inventory_count(Global.ResourceType.CORN, 1)
-	change_queue_count(Global.ResourceType.CORN, 1)
+	change_inventory_count(Global.ResourceType.FISH, 1)
+	change_queue_count(Global.ResourceType.FISH, 1)
 	change_queue_count(Global.ResourceType.CAT, -1)
 
 func handle_character(character: BaseCharacter):
 	if (character is Cat):
 		if (character.has_resource(Global.ResourceType.CAT)):
-			character.give_resource(Global.ResourceType.CORN)
-			change_inventory_count(Global.ResourceType.CORN, -1)
-			change_queue_count(Global.ResourceType.CORN, -1)
+			character.give_resource(Global.ResourceType.FISH)
+			change_inventory_count(Global.ResourceType.FISH, -1)
+			change_queue_count(Global.ResourceType.FISH, -1)
