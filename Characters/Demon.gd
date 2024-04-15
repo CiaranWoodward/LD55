4c9158ea7034598ca53a09b44073322242a83e18
Mai-Lan Vu <miavu11@gmail.com>
2024-04-15 21:50:50 +0100

    misc adjustments, drew bug and star icon

--- Characters/Demon.gd
+++ Characters/Demon.gd
@@ -4,6 +4,7 @@ extends BaseCharacter
 @export var skin_colors: Array[Color] = [Color.BISQUE]
 @export var hair_colors: Array[Color] = [Color.CORAL]
 @export var horn_colors: Array[Color] = [Color.SKY_BLUE]
+@export var eye_colors: Array[Color] = [Color.SKY_BLUE]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,6 +15,7 @@ func _restyle():
 	$Graphic/Character/Skin.self_modulate = skin_colors.pick_random()
 	$Graphic/Character/Fur.self_modulate = hair_colors.pick_random()
 	$Graphic/Character/Horns.self_modulate = horn_colors.pick_random()
+	$Graphic/Character/Body.self_modulate = eye_colors.pick_random()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
